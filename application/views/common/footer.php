98270cab13f2e5bc9893508f71931aeeced45c4e
Kartik <kartikchaudhari456@gmail.com>
2023-01-03 18:10:49 +0530

    removed marquee title

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -11,11 +11,5 @@
 <!--  bootstrap javascripts-->
 <script type="text/javascript" src="<?php echo base_url(JQUERY); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url(BOOTSTRAP_JS); ?>"></script>
-<script type="text/javascript">
-(function titleMarquee() {
-    document.title = document.title.substring(1)+document.title.substring(0,1);
-    setTimeout(titleMarquee, 200);
-})();
-</script>
 </body>
 </html>
